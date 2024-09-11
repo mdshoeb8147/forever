fdccf95955718e422a5058dddc7dc9c590fdc2d0
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-09-11 22:51:44 +0530

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets.js";
 import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
+  const [visible, setVisible] = useState(false); // Corrected
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} alt="" />
@@ -47,6 +50,42 @@ const Navbar = () => {
             10
           </p>
         </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt=""
+        />
+      </div>
+      {/* sidebar menu for side screen */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600 ">
+          <div
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink className="py-2 pl-6 border" to="/">
+            HOME
+          </NavLink>
+          <NavLink className="py-2 pl-6 border" to="/collection">
+            COLLECTION
+          </NavLink>
+          <NavLink className="py-2 pl-6 border" to="/about">
+            ABOUT
+          </NavLink>
+          <NavLink className="py-2 pl-6 border" to="/contact">
+            CONTACT
+          </NavLink>
+        </div>
       </div>
     </div>
   );

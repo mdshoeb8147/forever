d6ff7cb5e5ad70405c084f6a91e55faa38c719e8
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-10-10 20:10:07 +0530

    add logic on payment method

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/frontend_assets/assets";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -93,7 +93,10 @@ const Cart = () => {
         <div className="w-full sm:w-[450px]">
           <CartTotal />
           <div className="w-full text-end">
-            <button className="bg-black text-white text-sm my-8 px-8 py-3">
+            <button
+              onClick={() => navigate("/place-order")}
+              className="bg-black text-white text-sm my-8 px-8 py-3"
+            >
               PROCEED TO CHECKOUT
             </button>
           </div>

bc884c98ebef5e2e93a8e765d3f25d86b314fc8b
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-10-14 16:32:40 +0530

    completed with about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,66 @@
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
+import Title from "../components/Title";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-400">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quia
+            distinctio placeat tempore velit id corrupti veritatis sit quod
+            numquam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quia
+            distinctio placeat tempore velit id corrupti veritatis sit quod
+            numquam?
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor alias
+            pariatur ut facilis asperiores repudiandae vero provident, libero
+            sint dolore.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            eos!
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            eos!
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Services:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            eos!
+          </p>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,4 +1,12 @@
+import Title from "../components/Title";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+    </div>
+  );
 };
 export default Contact;

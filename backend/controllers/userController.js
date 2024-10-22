e45d3bd67ef846bbdccb6380ee533e8fa71dfd6b
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-10-22 21:00:48 +0530

    hash password 60422

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,30 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
 // route for user login
 const loginUser = async (req, res) => {};
 // route for user register
 
 const registerUser = async (req, res) => {
-  res.json({ msg: "Register API Working" });
+  try {
+    const { name, email, password } = req.body;
+    // user already exists or not
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validate email format or password
+
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "please enter valid email" });
+    }
+    if (password.length < 0) {
+      return res.json({
+        success: false,
+        message: "please enter strong password",
+      });
+    }
+  } catch (error) {}
 };
 
 // route for admin user

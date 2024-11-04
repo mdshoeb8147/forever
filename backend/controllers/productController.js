12e17c47bdfef8a339609559b9efd592b2acb6fe
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-11-04 21:48:29 +0530

    64054 product add api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,43 @@
 // function fro add product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
+    const image1 = req.files.image1[0];
+    const image2 = req.files.image2[0];
+    const image3 = req.files.image3[0];
+    const image4 = req.files.image4[0];
 
-const addProduct = async (req, res) => {};
+    console.log(
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller
+    );
+    console.log(image1, image2, image3, image4);
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 // function fro list product
-
 const listProduct = async (req, res) => {};
 
 // function fro remove product
-
 const removeProduct = async (req, res) => {};
-// function fro single product info
 
+// function fro single product info
 const singleProduct = async (req, res) => {};
 
 export { listProduct, addProduct, removeProduct, singleProduct };

c0a7178a22a900fb995f55897d2417ac75323f23
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-11-04 21:12:12 +0530

    error resolved for login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,7 +18,7 @@ const loginUser = async (req, res) => {
     }
     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
+    if (isMatch) {
       const token = createToken(user._id);
       res.json({ success: true, token });
     } else {

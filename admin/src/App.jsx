17ded948fc99a99b4a3d74b79c24d49def2e3dc0
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-11-13 21:22:22 +0530

    admin session localstorage

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,16 +4,28 @@ import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 const App = () => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
     <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
       {token === "" ? (
-        <Login />
+        <Login setToken={setToken} />
       ) : (
         <>
           <Navbar />

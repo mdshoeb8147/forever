d661c9320887dd0a9eff2675c590296ff4cf078d
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-11-26 21:51:19 +0530

    storing images in state variable

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,25 +1,74 @@
+import { useState } from "react";
 import { assets } from "../assets/assets";
 const Add = () => {
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("Men");
+  const [subCategory, setSubcategory] = useState("Topwear");
+  const [bestseller, setBestseller] = useState(false);
+  const [sizes, setSizes] = useState([]);
   return (
     <from className="flex flex-col w-full items-start gap-3">
       <div>
         <p className="mb-2">Upload Image</p>
         <div className="flex gap-2">
           <label htmlFor="image1">
-            <img className="w-20" src={assets.upload_area} alt="" />
-            <input type="file" id="image1" hidden />
+            <img
+              className="w-20"
+              src={!image1 ? assets.upload_area : URL.createObjectURL(image1)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage1(e.target.files[0])}
+              type="file"
+              id="image1"
+              hidden
+            />
           </label>
           <label htmlFor="image2">
-            <img className="w-20" src={assets.upload_area} alt="" />
-            <input type="file" id="image2" hidden />
+            <img
+              className="w-20"
+              src={!image2 ? assets.upload_area : URL.createObjectURL(image2)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage2(e.target.files[0])}
+              type="file"
+              id="image2"
+              hidden
+            />
           </label>
           <label htmlFor="image3">
-            <img className="w-20" src={assets.upload_area} alt="" />
-            <input type="file" id="image3" hidden />
+            <img
+              className="w-20"
+              src={!image3 ? assets.upload_area : URL.createObjectURL(image3)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage3(e.target.files[0])}
+              type="file"
+              id="image3"
+              hidden
+            />
           </label>
           <label htmlFor="image4">
-            <img className="w-20" src={assets.upload_area} alt="" />
-            <input type="file" id="image4" hidden />
+            <img
+              className="w-20"
+              src={!image4 ? assets.upload_area : URL.createObjectURL(image4)}
+              alt=""
+            />
+            <input
+              onChange={(e) => setImage4(e.target.files[0])}
+              type="file"
+              id="image4"
+              hidden
+            />
           </label>
         </div>
       </div>
